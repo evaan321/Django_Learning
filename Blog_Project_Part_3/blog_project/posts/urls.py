be35b78746299b4c88f908f94cafa74d0c2ad5c4
evaan321 <you@example.com>
2024-01-02 13:43:43 +0600

    added all

---
+++ Blog_Project_Part_3/blog_project/posts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('add/', views.AddPostCreateView.as_view(), name='add_post'),
+    path('edit/<int:id>/', views.EditPostView.as_view(), name='edit_post'),
+    path('delete/<int:id>/', views.DeletePost.as_view(), name='delete_post'),
+]
--- blog_project/profiles/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import *
-
-class profileForm(forms.ModelForm):
-
-    class Meta:
-        model = Profile
-        fields = '__all__'
